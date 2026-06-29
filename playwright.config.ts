2e55bfd0e45151d5bec7f5118a3b4b306c108eff
Tamas Loki <Tamas.Loki@wolterskluwer.com>
2026-06-29 13:33:35 +0200

    adds allure reporting, updates github actions flow

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   ...(process.env.CI ? { workers: 1 } : {}),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }], ['list']],
+    reporter: [
+    ['line'],
+    ['allure-playwright', { resultsDir: 'allure-results' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
