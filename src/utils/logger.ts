fa33779c2432c44b6fd8f9cb6f0bd684c4f82d52
Tamas Loki <Tamas.Loki@wolterskluwer.com>
2026-08-26 11:54:22 +0200

    update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,9 +6,11 @@ export class Logger {
   }
 
   info(...args: unknown[]): void {
+    // eslint-disable-next-line no-console
     console.info(`[INFO] ${this.context}`, ...args);
   }
   debug(...args: unknown[]): void {
+    // eslint-disable-next-line no-console
     console.debug(`[DEBUG] ${this.context}`, ...args);
   }
   warn(...args: unknown[]): void {
